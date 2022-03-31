de3300b2bbea5fdf5ac0f9d0b0163c06b97e3605
Logon PF Fiap <logonpflocal@fiap.com.br>
2022-03-31 20:58:14 -0300

    Beans

---
+++ progamer/src/main/java/br/com/fiap/model/Setup.java
@@ -0,0 +1,42 @@
+package br.com.fiap.model;
+
+import java.math.BigDecimal;
+
+public class Setup {
+
+	private String name = "Meu Setup";
+	private String description = "descrição do setup";
+	private BigDecimal price = new BigDecimal(2000);
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Setup [name=" + name + ", description=" + description + ", price=" + price + "]";
+	}
+	
+	
+
+}
